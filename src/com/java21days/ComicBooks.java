9f8e8a2f0746fbcd5cdb1cebbb35b54f94783be5
Alejandro Salas <asalas.wd@outlook.com>
2022-11-06 21:16:36 -0500

    Define price and condition for hash map

--- src/com/java21days/ComicBooks.java
+++ src/com/java21days/ComicBooks.java
@@ -1,5 +1,16 @@
 package com.java21days;
 
+import java.util.HashMap;
+
 public class ComicBooks {
 
+    public ComicBooks() {
+    }
+
+    public static void main(String[] args) {
+//        set up hash map
+        HashMap quality = new HashMap();
+        float[] prices = { 3.00F, 2.00F, 1.50F, 1.00F, 0.50F, 0.25F };
+        String[] condition = { "mint", "near mint", "very fine", "fine", "good", "poor" };
+    }
 }
