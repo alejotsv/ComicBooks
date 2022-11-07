a860ed445a1dbd4445f7a21b6049ba17226b8098
Alejandro Salas <asalas.wd@outlook.com>
2022-11-06 21:20:23 -0500

    Create hash map with quality and price

--- src/com/java21days/ComicBooks.java
+++ src/com/java21days/ComicBooks.java
@@ -10,7 +10,11 @@ public class ComicBooks {
     public static void main(String[] args) {
 //        set up hash map
         HashMap quality = new HashMap();
-        float[] prices = { 3.00F, 2.00F, 1.50F, 1.00F, 0.50F, 0.25F };
         String[] condition = { "mint", "near mint", "very fine", "fine", "good", "poor" };
+        float[] prices = { 3.00F, 2.00F, 1.50F, 1.00F, 0.50F, 0.25F };
+        for(int i=0; i<condition.length; i++) {
+            quality.put(condition[i], prices[i]);
+        }
+
     }
 }
