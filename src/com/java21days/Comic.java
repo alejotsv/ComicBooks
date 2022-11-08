11311c9087a54d6cb069ec8a8817b76f2ad7ce0e
Alejandro Salas <asalas.wd@outlook.com>
2022-11-07 20:35:55 -0500

    Set constructor for Comic class

--- src/com/java21days/Comic.java
+++ src/com/java21days/Comic.java
@@ -1,4 +1,15 @@
 package com.java21days;
 
 public class Comic {
+    String title;
+    String issue;
+    String condition;
+    float originalPrice;
+
+    Comic(String title, String issue, String condition, float originalPrice){
+        this.title = title;
+        this.issue = issue;
+        this.condition = condition;
+        this.originalPrice = originalPrice;
+    }
 }
