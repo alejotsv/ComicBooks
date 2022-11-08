9d358396693e39a67a0c9e9f56da4faa8f8f8f31
Alejandro Salas <asalas.wd@outlook.com>
2022-11-07 21:23:41 -0500

    Update quality HashMap to use Generics

--- src/com/java21days/ComicBooks.java
+++ src/com/java21days/ComicBooks.java
@@ -9,7 +9,7 @@ public class ComicBooks {
 
     public static void main(String[] args) {
 //        set up hash map
-        HashMap quality = new HashMap();
+        HashMap<String, Float> quality = new HashMap<>();
         String[] condition = { "mint", "near mint", "very fine", "fine", "good", "poor" };
         float[] prices = { 3.00F, 2.00F, 1.50F, 1.00F, 0.50F, 0.25F };
         for(int i=0; i<condition.length; i++) {
@@ -25,7 +25,7 @@ public class ComicBooks {
         int length = comix.length;
 
         for(int i=0; i<length; i++){
-            float price = (Float) quality.get(comix[i].condition);
+            float price = quality.get(comix[i].condition);
             comix[i].setFinalPrice(price);
             System.out.println("Title: " + comix[i].title);
             System.out.println("Issue number: " + comix[i].issue);
